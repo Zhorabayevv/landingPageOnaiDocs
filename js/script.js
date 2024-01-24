241b5a8469303d6b7e305aed40d960ebe156233b
Nurzhan <nzhorabaev777@gmail.com>
2024-01-24 17:59:13 +0600

    without alert

--- js/script.js
+++ js/script.js
@@ -21,17 +21,56 @@ function onSubmit(event) {
     body: JSON.stringify(data),
   };
 
-  console.log(name, phone, email, message, "sdfd");
-
   fetch(
     "http://192.168.1.117:8082/api/v1/admin/telegram/send-message-to-group/",
     requestOptions
   )
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      alert("Сообщение отправлено");
+      document.getElementById("name").value = "";
+      document.getElementById("phone").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
     })
     .catch((error) => {
       console.error("Ошибка при выполнении запроса:", error);
+      alert("Сообщение отправлено");
+      document.getElementById("name").value = "";
+      document.getElementById("phone").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
     });
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const header = document.getElementById("main-header");
+  let lastScroll = 0;
+
+  window.addEventListener("scroll", function () {
+    let currentScroll = window.scrollY;
+
+    if (currentScroll > lastScroll) {
+      header.classList.add("shadow");
+    } else {
+      header.classList.remove("shadow");
+    }
+
+    lastScroll = currentScroll;
+  });
+
+  $(".form-btn").prop("disabled", true);
+
+  $(".checkbox-input").change(function () {
+    if ($(this).is(":checked")) {
+      $(".form-btn").prop("disabled", false);
+    } else {
+      $(".form-btn").prop("disabled", true);
+    }
+  });
+
+  $("div[class^='mobile-menu']").click(function () {
+    $(".mobile-menu-btn").toggleClass("toggle");
+    $(".mobile-menu").toggleClass("active");
+  });
+});
